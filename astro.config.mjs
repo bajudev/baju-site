1a88b8622c226ee91fe2813006cfa75a78d03e3b
Rohan <rohanmm@outlook.com>
2025-05-21 00:03:22 +0100

    cloudlfare sux

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  adapter: cloudflare()
 });
