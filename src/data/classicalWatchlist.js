16bbc0a73170630d98c1a6610882bb0e0dca80e7
Rohan <35273864+setofR@users.noreply.github.com>
2025-06-04 09:31:19 +0100

    update music

--- src/data/classicalWatchlist.js
+++ src/data/classicalWatchlist.js
@@ -1,160 +1,160 @@
 // src/data/pieces.js
 export const pieces = {
-    watched: [
-      {
-        date: "2024-08-08",
-        venue: "Royal Albert Hall",
-        performances: [
-          {
-            composer: "L. V. Beethoven",
-            piece: "Violin Concerto",
-            performer: "Tobias Fieldmann",
-            type: "violin solo"
-          },
-          {
-            composer: "E. Elgar",
-            piece: "Salut d'amour",
-            type: "encore"
-          },
-          {
-            composer: "Sarah Gibson",
-            piece: "warp & weft"
-          },
-          {
-            composer: "Brahms",
-            piece: "Symphony No. 4 in E minor"
-          }
-        ]
-      },
-      {
-        date: "2025-02-08",
-        venue: "Birmingham Symphony Hall",
-        performances: [
-          {
-            composer: "Shostakovich",
-            piece: "Festive Overture"
-          },
-          {
-            composer: "Tchaikovsky",
-            piece: "Violin Concerto",
-            performer: "James Ehnes",
-            type: "violin"
-          },
-          {
-            composer: "Bach",
-            piece: "Violin Sonata No.2 in A minor III. Andante",
-            type: "encore"
-          },
-          {
-            composer: "Tchaikovsky",
-            piece: "Symphony No. 4"
-          }
-        ]
-      },
-      {
-        date: "2025-03-29",
-        venue: "Birmingham Symphony Hall",
-        performances: [
-          {
-            composer: "Tchaikovsky",
-            piece: "Fantasy Overture, Romeo and Juliet"
-          },
-          {
-            composer: "Bruch",
-            piece: "Violin Concerto No. 1",
-            performer: "Esther Yoo",
-            type: "violin"
-          },
-          {
-            composer: "Prokofiev",
-            piece: "Symphony No. 5"
-          }
-        ]
-      },
-      {
-        date: "2025-04-03",
-        venue: "Birmingham Symphony Hall",
-        performances: [
-          {
-            composer: "Arvo Pärt",
-            piece: "Our Garden"
-          },
-          {
-            composer: "Sibelius",
-            piece: "Violin Concerto",
-            performer: "Alina Ibragimova",
-            type: "violin"
-          },
-          {
-            composer: "Sibelius",
-            piece: "Finlandia"
-          },
-          {
-            composer: "Dvořák",
-            piece: "Symphony No.8"
-          }
-        ]
-      }
-    ],
-    wishlist: [
-      {
-        composer: "Paganini",
-        piece: "Violin Concerto No. 1 in E-flat major",
-        type: "violin"
-      },
-      {
-        composer: "Ravel",
-        piece: "Tzigane",
-        type: "violin"
-      },
-      {
-        composer: "Tchaikovsky",
-        piece: "Violin Concerto in D major",
-        type: "violin"
-      },
-      {
-        composer: "Ernst",
-        piece: "The Last Rose of Summer",
-        type: "violin"
-      },
-      {
-        composer: "Mendelssohn",
-        piece: "Violin Concerto in E minor",
-        type: "violin"
-      },
-      {
-        composer: "Ysaÿe",
-        piece: "Violin Sonata No. 3 Ballade",
-        type: "violin"
-      },
-      {
-        composer: "Wieniawski",
-        piece: "Polonaise Brillante",
-        type: "violin"
-      },
-      {
-        composer: "Dvořák",
-        piece: "9th Symphony"
-      },
-      {
-        composer: "Sibelius",
-        piece: "Violin Concerto in D minor",
-        type: "violin"
-      },
-      {
-        composer: "Bruch",
-        piece: "Violin Concerto No. 1 in G minor",
-        type: "violin"
-      },
-      {
-        composer: "Brahms",
-        piece: "Violin Concerto in D major",
-        type: "violin"
-      },
-      {
-        composer: "Mozart",
-        piece: "Violin Concerto No. 5 in A major",
-        type: "violin"
-      }
-    ]
-   };
+  watched: [
+    {
+      date: "2024-08-08",
+      venue: "Royal Albert Hall",
+      performances: [
+        {
+          composer: "L. V. Beethoven",
+          piece: "Violin Concerto",
+          performer: "Tobias Fieldmann",
+          type: "violin solo",
+        },
+        {
+          composer: "E. Elgar",
+          piece: "Salut d'amour",
+          type: "encore",
+        },
+        {
+          composer: "Sarah Gibson",
+          piece: "warp & weft",
+        },
+        {
+          composer: "Brahms",
+          piece: "Symphony No. 4 in E minor",
+        },
+      ],
+    },
+    {
+      date: "2025-02-08",
+      venue: "Birmingham Symphony Hall",
+      performances: [
+        {
+          composer: "Shostakovich",
+          piece: "Festive Overture",
+        },
+        {
+          composer: "Tchaikovsky",
+          piece: "Violin Concerto",
+          performer: "James Ehnes",
+          type: "violin",
+        },
+        {
+          composer: "Bach",
+          piece: "Violin Sonata No.2 in A minor III. Andante",
+          type: "encore",
+        },
+        {
+          composer: "Tchaikovsky",
+          piece: "Symphony No. 4",
+        },
+      ],
+    },
+    {
+      date: "2025-03-29",
+      venue: "Birmingham Symphony Hall",
+      performances: [
+        {
+          composer: "Tchaikovsky",
+          piece: "Fantasy Overture, Romeo and Juliet",
+        },
+        {
+          composer: "Bruch",
+          piece: "Violin Concerto No. 1",
+          performer: "Esther Yoo",
+          type: "violin",
+        },
+        {
+          composer: "Prokofiev",
+          piece: "Symphony No. 5",
+        },
+      ],
+    },
+    {
+      date: "2025-04-03",
+      venue: "Birmingham Symphony Hall",
+      performances: [
+        {
+          composer: "Arvo Pärt",
+          piece: "Our Garden",
+        },
+        {
+          composer: "Sibelius",
+          piece: "Violin Concerto",
+          performer: "Alina Ibragimova",
+          type: "violin",
+        },
+        {
+          composer: "Sibelius",
+          piece: "Finlandia",
+        },
+        {
+          composer: "Dvořák",
+          piece: "Symphony No.8",
+        },
+      ],
+    },
+  ],
+  wishlist: [
+    {
+      composer: "Paganini",
+      piece: "Violin Concerto No. 1 in E-flat major",
+      type: "violin",
+    },
+    {
+      composer: "Ravel",
+      piece: "Tzigane",
+      type: "violin",
+    },
+    {
+      composer: "Tchaikovsky",
+      piece: "Violin Concerto in D major",
+      type: "violin",
+    },
+    {
+      composer: "Ernst",
+      piece: "The Last Rose of Summer",
+      type: "violin",
+    },
+    {
+      composer: "Mendelssohn",
+      piece: "Violin Concerto in E minor",
+      type: "violin",
+    },
+    {
+      composer: "Ysaÿe",
+      piece: "Violin Sonata No. 3 Ballade",
+      type: "violin",
+    },
+    {
+      composer: "Wieniawski",
+      piece: "Polonaise Brillante",
+      type: "violin",
+    },
+    {
+      composer: "Dvořák",
+      piece: "9th Symphony",
+    },
+    {
+      composer: "Sibelius",
+      piece: "Violin Concerto in D minor",
+      type: "violin",
+    },
+    {
+      composer: "Bruch",
+      piece: "Violin Concerto No. 1 in G minor",
+      type: "violin",
+    },
+    {
+      composer: "Brahms",
+      piece: "Violin Concerto in D major",
+      type: "violin",
+    },
+    {
+      composer: "Mozart",
+      piece: "Violin Concerto No. 5 in A major",
+      type: "violin",
+    },
+  ],
+};
